17052bce35a6777e9199968e35383645ee36170d
abdelgawad hamdy <abdelgawadhamdy2015@gmail.com>
2024-07-31 13:27:32 +0300

    run on ios

--- lib/core/widgets/my_app_bar.dart
+++ lib/core/widgets/my_app_bar.dart
@@ -60,12 +60,9 @@ class _MyAppBarState extends State<MyAppBar> {
               color: Colors.white,
             ),
             // horozintalSpacing(MediaQuery.of(context).size.width * .05),
-            FittedBox(
-              fit: BoxFit.cover,
-              child: Image.asset(
-                "images/smaller_apex.png",
-                width: MediaQuery.of(context).size.width * .15,
-              ),
+            Image.asset(
+              "images/smaller_apex.png",
+              width: MediaQuery.of(context).size.width * .15,
             ),
             const Spacer(),
             const Icon(Icons.notifications_none_outlined),
