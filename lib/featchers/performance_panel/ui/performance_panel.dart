47d8fcf95ee714c8627fb6797cb7fd07b0444b2a
abdelgawad hamdy <abdelgawadhamdy2015@gmail.com>
2024-09-01 10:09:54 +0300

    add print uses case to manifest file

--- lib/featchers/performance_panel/ui/performance_panel.dart
+++ lib/featchers/performance_panel/ui/performance_panel.dart
@@ -10,6 +10,7 @@ import 'package:ttech_attendance/core/widgets/app_bar/my_drawer.dart';
 import 'package:ttech_attendance/featchers/performance_panel/data/models/performance_employee_model.dart';
 import 'package:ttech_attendance/featchers/performance_panel/data/models/performance_employee_response.dart';
 import 'package:ttech_attendance/featchers/performance_panel/logic/cubit/performance_employee_cubit.dart';
+import 'package:ttech_attendance/featchers/performance_panel/logic/cubit/performance_employee_state.dart';
 import 'package:ttech_attendance/featchers/performance_panel/ui/widget/performance_block_listener.dart';
 import 'package:ttech_attendance/featchers/performance_panel/ui/widget/perfromance_list_view.dart';
 import 'package:ttech_attendance/generated/l10n.dart';
@@ -63,6 +64,11 @@ class _PerformancePanel extends State<PerformancePanel> {
   }
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     days = context.read<PerformanceEmployeeCubit>().datalist.isNotEmpty
         ? context
@@ -74,75 +80,90 @@ class _PerformancePanel extends State<PerformancePanel> {
             .days
         : [];
     return Scaffold(
-        appBar: MyAppBar(
-          changeLanguage: widget.changeLanguage,
-          context: context,
-          title: MyConstants.attendanceAndDepartureReports,
-        ),
-        drawer: const Drawer(
-          child: MyDrawer(),
-        ),
-        backgroundColor: ColorManger.lightGray,
-        body:  SafeArea(
-            child: Container(
-              color: ColorManger.lightGray,
-              padding: SizeConfig().getScreenPadding(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      appBar: MyAppBar(
+        changeLanguage: widget.changeLanguage,
+        context: context,
+        title: MyConstants.attendanceAndDepartureReports,
+      ),
+      drawer: const Drawer(
+        child: MyDrawer(),
+      ),
+      backgroundColor: ColorManger.lightGray,
+      body: SafeArea(
+        child: Container(
+          color: ColorManger.lightGray,
+          padding: SizeConfig().getScreenPadding(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
                 children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () => _selectDate(context, true),
-                          child: AbsorbPointer(
-                            child: TextFormField(
-                              style: TextStyles.font12black54Reguler,
-                              controller: TextEditingController(
-                                text: _startDate != null
-                                    ? MyConstants.dateFormat.format(_startDate!)
-                                    : S.of(context).fromDate,
-                              ),
-                              decoration: InputDecoration(
-                                  labelText: S.of(context).fromDate,
-                                  hintText: _startDate != null
-                                      ? MyConstants.dateFormat.format(_startDate!)
-                                      : S.of(context).fromDate,
-                                  hintStyle: TextStyles.font12black54Reguler,
-                                  labelStyle: TextStyles.font12black54Reguler),
-                            ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () => _selectDate(context, true),
+                      child: AbsorbPointer(
+                        child: TextFormField(
+                          style: TextStyles.font12black54Reguler,
+                          controller: TextEditingController(
+                            text: _startDate != null
+                                ? MyConstants.dateFormat.format(_startDate!)
+                                : S.of(context).fromDate,
                           ),
+                          decoration: InputDecoration(
+                              labelText: S.of(context).fromDate,
+                              hintText: _startDate != null
+                                  ? MyConstants.dateFormat.format(_startDate!)
+                                  : S.of(context).fromDate,
+                              hintStyle: TextStyles.font12black54Reguler,
+                              labelStyle: TextStyles.font12black54Reguler),
                         ),
                       ),
-                      const PerformanceBlockListener(),
-                      horizontalSpacing(SizeConfig.screenWidth! * .1),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () => _selectDate(context, false),
-                          child: AbsorbPointer(
-                            child: TextFormField(
-                              style: TextStyles.font12black54Reguler,
-                              controller: TextEditingController(
-                                text: _endDate != null
-                                    ? MyConstants.dateFormat.format(_endDate!)
-                                    : S.of(context).toDate,
-                              ),
-                              decoration: InputDecoration(
-                                  labelText: S.of(context).toDate,
-                                  hintText: _endDate != null
-                                      ? MyConstants.dateFormat.format(_endDate!)
-                                      : S.of(context).toDate,
-                                  hintStyle: TextStyles.font12black54Reguler,
-                                  labelStyle: TextStyles.font12black54Reguler),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                  verticalSpacing(SizeConfig.screenHeight! * .02),
-                  Center(
-                    child: Row(
+                  const PerformanceBlockListener(),
+                  horizontalSpacing(SizeConfig.screenWidth! * .1),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () => _selectDate(context, false),
+                      child: AbsorbPointer(
+                        child: TextFormField(
+                          style: TextStyles.font12black54Reguler,
+                          controller: TextEditingController(
+                            text: _endDate != null
+                                ? MyConstants.dateFormat.format(_endDate!)
+                                : S.of(context).toDate,
+                          ),
+                          decoration: InputDecoration(
+                              labelText: S.of(context).toDate,
+                              hintText: _endDate != null
+                                  ? MyConstants.dateFormat.format(_endDate!)
+                                  : S.of(context).toDate,
+                              hintStyle: TextStyles.font12black54Reguler,
+                              labelStyle: TextStyles.font12black54Reguler),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              verticalSpacing(SizeConfig.screenHeight! * .02),
+              Center(
+                child: BlocBuilder<PerformanceEmployeeCubit,
+                    PerformanceEmployeeState>(
+                  builder: (context, state) {
+                    days = context
+                            .read<PerformanceEmployeeCubit>()
+                            .datalist
+                            .isNotEmpty
+                        ? context
+                            .read<PerformanceEmployeeCubit>()
+                            .datalist
+                            .first
+                            .employees!
+                            .first
+                            .days
+                        : [];
+                    return Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         IconButton(
@@ -152,6 +173,9 @@ class _PerformancePanel extends State<PerformancePanel> {
                                     days!,
                                     "${S.of(context).transaction} ${S.of(context).from} $_startDate ${S.of(context).to} $_endDate",
                                     context);
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text(" no data ")));
                               }
                             },
                             icon: const Icon(Icons.print)),
@@ -165,14 +189,16 @@ class _PerformancePanel extends State<PerformancePanel> {
                         const Spacer(),
                         Text("0", style: TextStyles.font12black54Reguler),
                       ],
-                    ),
-                  ),
-                  verticalSpacing(SizeConfig.screenHeight! * .01),
-                  PerfromanceListView()
-                ],
+                    );
+                  },
+                ),
               ),
-            ),
+              verticalSpacing(SizeConfig.screenHeight! * .01),
+              PerfromanceListView()
+            ],
           ),
-        );
+        ),
+      ),
+    );
   }
 }
